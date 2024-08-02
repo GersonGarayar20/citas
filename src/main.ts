999c7ba362ee694b6dbac96a6b680607901bc40e
gerson <garayargerson20@gmail.com>
2024-08-02 14:47:44 -0500

    fix: renamed documentation route from 'documentation' to 'doc

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
     .addTag('users')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('documentation', app, document);
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(4000);
 }
